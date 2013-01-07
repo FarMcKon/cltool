4cda25fa7a4b667a7b2a10e77f7d4239baabb950
Far McKon <FarMcKon@gmail.com>
2013-01-07 17:04:29 -0500

    initial build of cltool test project

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+print "setup.py happening"
+from setuptools import setup, find_packages
+
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.build_py import build_py
+from distutils.command.build_scripts import build_scripts
+ 
+# Post install object class, extends behavior
+class post_install(install_data):
+        def run(self):
+            print "Custom install_data created"
+            # Call parent 
+            install_data.run(self)
+            # Execute commands
+            print "Running Custom Post Install"
+        def append_to_bashrc(self):
+            print "FUUUUUUUCK yeah. Appending myself to .bashrc"
+            import pdb
+            pdb.set_trace()
+
+setup(
+    name='cltool',
+    version='0.5.0.3',
+    description='example of installing a comand line tool via pip/easy_install',
+    cmdclass={"install_data": post_install},
+    packages=find_packages(where='.'),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['scripts/cltool','scripts/cltool2'],
+)
+   
