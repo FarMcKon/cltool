4a5c3147b8689dfbf031b24a02d2ada78e133bc9
Far McKon <FarMcKon@gmail.com>
2013-01-08 08:16:40 -0500

    cleaned up a bunch of cruft. Expanded the README. Added __future__ imports where intellegent

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
-
-print "setup.py happening"
-from setuptools import setup, find_packages
-
-from distutils.core import setup
-from distutils.command.build_py import build_py
-from distutils.command.build_scripts import build_scripts
- 
-## Post install object class, extends behavior
-## I could not get this to work
-#from distutils.command.install_data import install_data
-#class post_install(install_data):
-#        def run(self):
-#            print "Custom install_data created"
-#            # Call parent 
-#            install_data.run(self)
-#            # Execute commands
-#            print "Running Custom Post Install"
-#        def append_to_bashrc(self):
-#            print "FUUUUUUUCK yeah. Appending myself to .bashrc"
-#            import pdb
-#            pdb.set_trace()
+from __future__ import ( print_function, with_statement, absolute_import )
+import sys
+# When possible, use distribute_setup's version of tools
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+except ImportError:
+    from setuptools import setup, find_packages
+if sys.version >= '2.3':
+  import re, os, sys
+  version = re.search(
+        "__version__.*'(.+)'",
+        open(os.path.join('cltool','__init__.py')).read()).group(1)
+else:
+    raise Exception("only python 2.3 or newer supported")
 
 setup(
     name='cltool',
-    version='0.5.0.4',
-    description='example of installing a comand line tool via pip/easy_install',
-    #cmdclass={"install_data": post_install},
+    version= version,
+    description='Example of installing a comand line tool via pip/easy_install',
     packages=find_packages(where='.'),
     include_package_data=True,
     zip_safe=False,
